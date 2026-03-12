626ec53d97b737889a6d8652937c0c330091aa3b
pooranibcbb <42645335+pooranibcbb@users.noreply.github.com>
2026-03-12 18:35:54 -0400

    remove locus files

--- biowulf_submit_vp.sh
+++ biowulf_submit_vp.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
-#$ -N run_vp
-#$ -cwd
-#$ -j y
-#$ -l h_vmem=8G
-#$ -o .
-#$ -m e
-#$ -M your.email@niaid.nih.gov
+### Biowulf params
+#SBATCH --job-name=run_sm
+#SBATCH --output=%x-%j.out
+#SBATCH --partition=norm
+#SBATCH --mail-type=ALL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --cpus-per-task=1              # CPUs
+#SBATCH --mem=8G                        # Job memory request
+#SBATCH --time=2-00:00:00
+#SBATCH --export=NONE
+#SBATCH --gres=lscratch:1
 
 
-module load snakemake || exit 1
+
+module load snakemake/7.32.4 || exit 1
 
 ## protect us from user libs
 export PYTHONNOUSERSITE=True
 
 ## project config
-configfile=/path/to/project_config.yaml
+configfile=/data/subramanianp4/test/project_config.yaml
 
 ## cluster config file
 clusterfile=$(grep "clusterfile" ${configfile} | awk '{ print $2 }')
@@ -29,13 +33,17 @@ mkdir -p ${outputdir}
 scriptdir=$(grep "scriptdir" ${configfile} | awk '{ print $2 }')
 
 ## job submit command - can't use drmaa on locus :( - would have to ask them to install it...
-clustercmd="qsub -l h_vmem={cluster.h_vmem} -j y -pe threaded {cluster.threads} {cluster.extra} -o ${outputdir}/{cluster.log}"
+sbatchcmd="sbatch -c {cluster.threads} --mem={cluster.mem} --output=${outputdir}/{cluster.log}/%x-%j.out --partition={cluster.partition} --time={cluster.time} {cluster.extra}"
+
 
 ## add --dryrun to test
 ## sometimes useful to add --ignore-incomplete if snakemake's incomplete checking is not working for you.
 ## and --rerun-triggers mtime if you are debugging and don't want to keep re-running rules when code/format is changed but output is unchanged
 snakemake -s ${scriptdir}/Snakefile --jobs 32 --jobname "{name}.{cluster.jobname}.{jobid}" \
 	  --configfile ${configfile} \
-	  --cluster "${clustercmd}" --cluster-config ${clusterfile} --cluster-cancel "qdel" \
+	  --cluster "${sbatchcmd}" \
+	  --cluster-config ${clusterfile} \
+	  --cluster-cancel "scancel" \
+	  --latency-wait 120 --max-jobs-per-second 1 \
 	  --nolock --keep-going --keep-incomplete --use-envmodules \
 	  all
